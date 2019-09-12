8aebeb9f4b57c39c6db2dddcdfc8919115b48252
Weeboga <rathboglarka@gmail.com>
2019-09-12 14:15:23 +0200

    up

--- ts/content.ts
+++ ts/content.ts
@@ -13,7 +13,23 @@ export default class Content {
         // }
 
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<!DOCTYPE html>");
+        res.write("<html lang='hu'>");
+        res.write("<html>");
+        // a weboldal fejléce
+        res.write("<head>");
+        res.write("<title>TéglalapTK</title>");
+
+        res.write("</head>");
+        res.write("<body>");
         res.write("<h1>Téglalap területe és kerülete</h1>");
+
+        const a = 5;
+        const b = 3;
+        res.write(`<h2>a=${a}</h2>`);
+        res.write(`<h2>b=${b}</h2>`);
+        res.write("</body>");
+        res.write("</html>");
         res.end();
     }
 }
