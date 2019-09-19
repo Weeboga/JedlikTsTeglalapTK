5933773bbffd7b2414f276c4f6b0b847d2a1fb69
Weeboga <rathboglarka@gmail.com>
2019-09-19 13:05:59 +0200

    1

--- ts/content.ts
+++ ts/content.ts
@@ -1,16 +1,13 @@
-﻿import * as fs from "fs";
+﻿
 import * as http from "http";
-import * as path from "path";
+import { ParsedUrlQuery } from "querystring";
+import * as url from "url";
+
 
 export default class Content {
 
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
-        // favicon.ico kérés kiszolgálása:
-        // if (req.url === "/favicon.ico") {
-        //     res.writeHead(200, { "Content-Type": "image/x-icon" });
-        //     fs.createReadStream("favicon.ico").pipe(res);
-        //     return;
-        // }
+
 
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         res.write("<!DOCTYPE html>");
@@ -21,14 +18,26 @@ export default class Content {
         res.write("<title>TéglalapTK</title>");
 
         res.write("</head>");
-        res.write("<body>");
+
+        res.write("<body><form style = 'font-family:Courier; font-size:24px;'>");
+
         res.write("<h1>Téglalap területe és kerülete</h1>");
 
-        const a = 5;
-        const b = 3;
-        res.write(`<h2>a=${a}</h2>`);
-        res.write(`<h2>b=${b}</h2>`);
-        res.write("</body>");
+        const query: ParsedUrlQuery = url.parse(req.url, true).query;
+        const a = query.aInput === undefined || query.aInput === "" ? 5 : parseFloat(query.aInput as string);
+        const b = query.bInput === undefined || query.bInput === "" ? 5 : parseFloat(query.bInput as string);
+
+        res.write(`<p>a= `);
+        res.write(`<input type='number' name='aInput' value=${a} onChange='this.form.submit();'></p>`)
+        res.write(`<p>b= `);
+        res.write(`<input type='number' name='bInput' value=${a} onChange='this.form.submit();'></p>`)
+        const terulet: number = a * b;
+        const kerulet: number = 2 * (a + b);
+        res.write(`<p>Terület=${terulet}</p>`);
+        res.write(`<p>Kerület=${kerulet}</p>`);
+
+        res.write("</form></body>");
+
         res.write("</html>");
         res.end();
     }
